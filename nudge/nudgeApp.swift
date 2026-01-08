0c8988c16e91c95a2744ed0a9dc813bffa932d15
jna312 <jna312@gmail.com>
2026-01-08 16:54:41 -0500

    Initial Commit

---
+++ nudge/nudgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  nudgeApp.swift
+//  nudge
+//
+//  Created by Jeffrey on 1/8/26.
+//
+
+import SwiftUI
+
+@main
+struct nudgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
