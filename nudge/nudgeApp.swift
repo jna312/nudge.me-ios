fd3394945796e5709df4bec1a6119404293221c3
jna312 <jna312@gmail.com>
2026-01-13 10:44:45 -0500

    Add AppDelegate for portrait orientation lock (iOS 26 compatible)
    Uses UIApplicationDelegate to programmatically lock app to portrait.
    This approach works without UIRequiresFullScreen deprecation warnings.

--- nudge/nudgeApp.swift
+++ nudge/nudgeApp.swift
@@ -1,8 +1,16 @@
 import SwiftData
 import SwiftUI
 
+// MARK: - App Delegate for Orientation Lock
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        return .portrait
+    }
+}
+
 @main
 struct NudgeApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     @StateObject private var settings = AppSettings()
     
     let modelContainer: ModelContainer
